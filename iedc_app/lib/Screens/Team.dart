c2662600ddf5175c4290d2e014b0cbfc1898ef8c
jalala-pv <pvjalala@gmail.com>
2023-03-01 20:40:17 +0530

    ddd

--- iedc_app/lib/Screens/Team.dart
+++ iedc_app/lib/Screens/Team.dart
@@ -1,12 +1,33 @@
 import 'package:flutter/material.dart';
+
 class Team extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: SafeArea(child: 
-      Text('About')
+      body: SafeArea(
+        child: Center(
+          child: Stack(
+            clipBehavior: Clip.none,
+            children: [
+              Positioned(
+                  child: Container(
+                width: width / 1.5,
+                height: height / 5,
+                color: Colors.blue,
+              )),
+              Positioned(
+                  bottom: 70,
+                  left: 70,
+                  child: Container(
+                    width: width / 3,
+                    height: height / 4,
+                    color: Colors.amber,
+                  ))
+            ],
+          ),
+        ),
       ),
     );
   }
