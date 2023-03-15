1ad17741cb89cc37b19b2f1b35ff211195847b79
jalala-pv <pvjalala@gmail.com>
2023-03-15 16:21:07 +0530

    aa

--- iedc_app/lib/widgets/assets.dart
+++ iedc_app/lib/widgets/assets.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
-String logo = "assets/iedclogo.png";
-String flower = "assets/flower.png";
-String grp1 = "assets/grp1.jpg";
-String lab = "assets/lab.jpg";
-String lap = "assets/lap.jpg";
-String flu = "assets/flu.jpg";
-String lab3= "assets/lab3.jpg";
-String visionn= "assets/visionn.jpg";
-String mission= "assets/mission.jpg";
-String vision= "assets/vision.jpg";
-String idea= "assets/idea.jpeg";
-String cert= "assets/cert.jpg";
+ String logo = "assets/App_images/iedclogo.png";
+ String flower = "assets/App_images/flower.png";
+String grp1 = "assets/App_images/grp1.jpg";
+String lab = "assets/App_images/lab.jpg";
+String lap = "assets/App_images/lap.jpg";
+String flu = "assetsApp_images//flu.jpg";
+String lab3= "assets/App_images/lab3.jpg";
+String visionn= "assets/App_images/visionn.jpg";
+String mission= "assets/App_images/mission.jpg";
+String vision= "assets/App_images/vision.jpg";
+String idea= "assets/App_images/idea.jpeg";
+String cert= "assets/App_images/cert.jpg";
